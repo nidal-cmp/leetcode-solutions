f24cd00bf6ed8d5246897bfc6649f47f17600fd6
nidal-cmp <nidalcmp@gmail.com>
2026-08-30 09:27:28 +0000

    Code challenge submission for question: 443 String Compression

---
+++ 443 String Compression/443string-compression.py
@@ -0,0 +1,24 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        
+        write = 0
+        read = 0
+
+        while read < len(chars):
+
+            current = chars[read]
+            count = 0
+
+            while read < len(chars) and chars[read] == current:
+                read += 1
+                count +=1
+
+            chars[write] = current
+            write += 1
+           
+            if count > 1:
+                for digit in str(count):
+                    chars[write] = digit
+                    write += 1
+
+        return write
