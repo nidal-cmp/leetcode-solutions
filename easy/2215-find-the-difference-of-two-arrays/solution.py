4fdcc2e2bb2bd921d013838568aa56c5f0cc7e2b
Nidal <nidalcmp@gmail.com>
2026-08-31 14:21:49 +0530

    ✅ Find the Difference of Two Arrays | Easy | Python | Runtime: 3 ms (94.22%) | Memory: 19.5 MB | 2026-08-31

--- easy/2215-find-the-difference-of-two-arrays/solution.py
+++ easy/2215-find-the-difference-of-two-arrays/solution.py
@@ -1,15 +1,18 @@
 class Solution:
-    def pivotIndex(self, nums: List[int]) -> int:
-        total = sum(nums)
-        left = 0
-        for i in range(len(nums)):
-            right = total - left - nums[i]
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        set1=set(nums1)
+        set2=set(nums2)
 
-            if right == left:
+        answer1 = []
+        answer2 = []
 
-               return i
-            
-            left += nums[i]
 
-        return -1
+        for num in set1:
+            if num not in set2:
+                answer1.append(num)
         
+        for num in set2:
+            if num not in set1:
+                answer2.append(num)
+        
+        return [answer1,answer2]
