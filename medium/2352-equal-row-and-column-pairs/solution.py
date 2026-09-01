cbf132905bd0c1e9478224d0c3c485ab2f209f01
Nidal <nidalcmp@gmail.com>
2026-09-01 17:23:46 +0530

    ✅ Equal Row and Column Pairs | Medium | Python | Runtime: 25 ms (51.20%) | Memory: 24.4 MB | 2026-09-01

--- medium/2352-equal-row-and-column-pairs/solution.py
+++ medium/2352-equal-row-and-column-pairs/solution.py
@@ -1,19 +1,30 @@
 class Solution:
-    def closeStrings(self, word1: str, word2: str) -> bool:
-        
+    def equalPairs(self, grid: List[List[int]]) -> int:
         count = {}
 
-        if set(word1) != set(word2):
-           
-           return False
+        for row in grid:
+            row = tuple(row)
+            count[row] = count.get(row,0)+1
         
-        count1 = {}
-        count2 = {}
+        answer = 0
 
-        for char in word1:
-            count1[char]=count1.get(char,0)+1
+        n = len(grid)
+        
+         
+        
+        for col in range(n):
+            column = []
 
-        for char in word2:
-            count2[char]=count2.get(char,0)+1
+            for row in range(n):
+                column.append(grid[row][col])
+            
+            column = tuple(column)
 
-        return sorted(count1.values()) == sorted(count2.values())
+            answer += count.get(column,0)
+
+        return answer
+
+
+         
+
+        
