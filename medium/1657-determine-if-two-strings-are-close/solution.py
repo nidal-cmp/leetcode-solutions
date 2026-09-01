5475cc5473e9674f33b21b67e4eb9df018c0fb96
Nidal <nidalcmp@gmail.com>
2026-09-01 17:03:26 +0530

    ✅ Determine if Two Strings Are Close | Medium | Python | Runtime: 133 ms (48.41%) | Memory: 20.5 MB | 2026-09-01

--- medium/1657-determine-if-two-strings-are-close/solution.py
+++ medium/1657-determine-if-two-strings-are-close/solution.py
@@ -1,11 +1,19 @@
 class Solution:
-    def uniqueOccurrences(self, arr: List[int]) -> bool:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        
         count = {}
 
-        for num in arr:
-            count[num] = count.get(num,0)+1
-
-        frequencies = list(count.values())
-
-        return len(frequencies) == len(set(frequencies))
+        if set(word1) != set(word2):
+           
+           return False
         
+        count1 = {}
+        count2 = {}
+
+        for char in word1:
+            count1[char]=count1.get(char,0)+1
+
+        for char in word2:
+            count2[char]=count2.get(char,0)+1
+
+        return sorted(count1.values()) == sorted(count2.values())
