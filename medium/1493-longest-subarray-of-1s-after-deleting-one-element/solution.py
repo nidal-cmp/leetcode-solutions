77c62ca816477e723c9dbc6f3f1d8651c4b238dc
Nidal <nidalcmp@gmail.com>
2026-09-07 14:40:47 +0530

    ✅ Longest Subarray of 1's After Deleting One Element | Medium | Python | Runtime: 43 ms (69.06%) | Memory: 24.5 MB | 2026-09-07

--- medium/1493-longest-subarray-of-1s-after-deleting-one-element/solution.py
+++ medium/1493-longest-subarray-of-1s-after-deleting-one-element/solution.py
@@ -1,24 +1,18 @@
 class Solution:
-    def longestOnes(self, nums: List[int], k: int) -> int:
-        l=0
-        nz=0
+    def longestSubarray(self, nums: List[int]) -> int:
+       l = 0
+       zeros = 0
+       m_w = 0
 
-        m_w=0
-
-        for r in range(len(nums)):
+       for r in range(len(nums)):
              if nums[r] == 0:
-               nz += 1
-            
-             while nz > k:
+               zeros += 1
+
+             while zeros > 1:
                  if nums[l] == 0:
-                    nz -= 1
-                 l += 1
+                    zeros -=1
+                 l+=1
+                
+             m_w=max(m_w,r-l)
 
-             w = r - l + 1
-
-             m_w = max(m_w,w)
-
-        return m_w
-
-
-        
+       return m_w
