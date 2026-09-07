a258cfe7feb9fd6b4e850342f20be45221f62201
Nidal <nidalcmp@gmail.com>
2026-09-07 11:56:54 +0530

    ✅ Max Consecutive Ones III | Medium | Python | Runtime: 51 ms (85.55%) | Memory: 22.1 MB | 2026-09-07

--- medium/1004-max-consecutive-ones-iii/solution.py
+++ medium/1004-max-consecutive-ones-iii/solution.py
@@ -1,22 +1,24 @@
 class Solution:
-    def maxVowels(self, s: str, k: int) -> int:
-        
-        vowels = 'aeiou'
-        count = 0
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        l=0
+        nz=0
 
-        for i in range(k):
-            if s[i] in vowels:
-                count+=1
+        m_w=0
+
+        for r in range(len(nums)):
+             if nums[r] == 0:
+               nz += 1
             
-        max_count=count
+             while nz > k:
+                 if nums[l] == 0:
+                    nz -= 1
+                 l += 1
 
-        for i in range(k,len(s)):
-            if s[i] in vowels:
-                count+=1
+             w = r - l + 1
 
-            if s[i-k] in vowels:
-                count-=1 
+             m_w = max(m_w,w)
 
-            max_count = max (max_count, count)
+        return m_w
 
-        return max_count
+
+        
